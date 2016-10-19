921e5de20198790d01359a3d3b8ece1f6c98e786
PC <PC@PC-PC>
2016-10-19 18:12:41 +0100

    First commit

--- src/Daos/BookStockDao.java
+++ src/Daos/BookStockDao.java
@@ -30,15 +30,16 @@ public class BookStockDao extends Dao implements BookStockDaoInterface {
         {
             con = getConnection();
             
-            String query = "Select * from bookStock";
+            String query = "Select * from bookstock";
             ps = con.prepareStatement(query);
             rs = ps.executeQuery();
             while(rs.next())
             {
                 BookStock book = new BookStock(rs.getInt("bookID") , rs.getString("bookName"), rs.getString("author"), rs.getString("publisher"), rs.getInt("copies"));
+                bookStock.add(book);
             }
         }catch (SQLException e) {
-            System.out.println("Exception occured in the getAllUsers() method: " + e.getMessage());
+            System.out.println("Exception occured in the getAllBooks() method: " + e.getMessage());
         } finally {
             try {
                 if (rs != null) {
@@ -51,18 +52,88 @@ public class BookStockDao extends Dao implements BookStockDaoInterface {
                     freeConnection(con);
                 }
             } catch (SQLException e) {
-                System.out.println("Exception occured in the finally section of the getAllUsers() method: " + e.getMessage());
+                System.out.println("Exception occured in the finally section of the getAllBooks() method: " + e.getMessage());
             }
     }
+        return bookStock;
+}
 
     @Override
     public ArrayList<BookStock> getABookByName(String bookName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<BookStock> bookStock = new ArrayList();
+        try
+        {
+            con = getConnection();
+            
+            String query = "Select * from bookstock where bookName = ? ";
+            ps = con.prepareStatement(query);
+            ps.setString(1, bookName);
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+                BookStock book = new BookStock(rs.getInt("bookID") , rs.getString("bookName"), rs.getString("author"), rs.getString("publisher"), rs.getInt("copies"));
+                bookStock.add(book);
+            }
+        }catch (SQLException e) {
+            System.out.println("Exception occured in the getABookByName() method: " + e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the getABookByName() method: " + e.getMessage());
+            }
+    }
+        return bookStock;
     }
 
     @Override
     public ArrayList<BookStock> getABookContainingName(String bookName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<BookStock> bookStock = new ArrayList();
+        try
+        {
+            con = getConnection();
+            
+            String query = "Select * from bookStock where bookName LIKE ? ";
+            ps = con.prepareStatement(query);
+            ps.setString(1,"%" +  bookName + "%");
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+                BookStock book = new BookStock(rs.getInt("bookID") , rs.getString("bookName"), rs.getString("author"), rs.getString("publisher"), rs.getInt("copies"));
+                bookStock.add(book);
+            }
+        }catch (SQLException e) {
+            System.out.println("Exception occured in the getABookContainingName() method: " + e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the getABookContainingName() method: " + e.getMessage());
+            }
+    }
+        return bookStock;
     }
 
     @Override
@@ -72,17 +143,123 @@ public class BookStockDao extends Dao implements BookStockDaoInterface {
 
     @Override
     public boolean RemoveABook(int bookID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement ps = null;
+        int rowsAffected = 0;
+        try
+        {
+            con = getConnection();
+            
+            String query = "DELETE FROM bookStock where bookID = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, bookID);
+            rowsAffected = ps.executeUpdate();
+            
+        }catch (SQLException e) {
+            System.out.println("Exception occured in the removeABook() method: " + e.getMessage());
+        } finally {
+            try {
+                
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the removeABook() method: " + e.getMessage());
+            }
+    }
+        if(rowsAffected < 0)
+        {
+            return true;
+        }
+        else
+            return false;
     }
 
     @Override
     public boolean AddingABook(BookStock b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement ps = null;
+        int rowsAffected = 0;
+        
+        try{
+            con = getConnection();
+
+            String query = "Insert into bookStock values(?, ?, ?, ?, ?)";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, b.getBookID());
+            ps.setString(2, b.getBookName());
+            ps.setString(3, b.getAuthor());
+            ps.setString(4, b.getPublisher());
+            ps.setInt(5, b.getCopies());
+       
+            rowsAffected = ps.executeUpdate(); 
+            
+        }catch (SQLException e) {
+            System.out.println("Exception occured in the AddingABook() method: " + e.getMessage());
+            
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the AddingABook() method");
+                e.getMessage();
+                
+            }
+        }
+        if(rowsAffected > 0)
+        {
+            return true;
+        }
+        else
+            return false;
     }
 
     @Override
     public boolean EditingABook(int bookID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement ps = null;
+        int rowsAffected = 0;
+        
+        try{
+            con = getConnection();
+
+            String query = "UPDATE bookstock set bookID WHERE bookID = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, bookID);
+         
+            rowsAffected = ps.executeUpdate(); 
+            
+        }catch (SQLException e) {
+            System.out.println("Exception occured in the EditingABook() method: " + e.getMessage());
+            
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the EditingABook() method");
+                e.getMessage();
+                
+            }
+        }
+        if(rowsAffected > 0)
+        {
+            return true;
+        }
+        else
+            return false;
     }
 
     @Override
