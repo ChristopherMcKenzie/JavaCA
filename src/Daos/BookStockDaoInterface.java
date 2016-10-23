6e109c259519f79daf29f6fecd4e96effe0a4ff0
PC <PC@PC-PC>
2016-10-23 10:48:05 +0100

    commit

--- src/Daos/BookStockDaoInterface.java
+++ src/Daos/BookStockDaoInterface.java
@@ -30,7 +30,7 @@ public interface BookStockDaoInterface {
     //should remove a book from libary (record compeltly removed)(admin only)
     public boolean AddingABook(BookStock b);
     //Adding a book to database
-    public boolean EditingABook(int bookID);
+    public boolean EditingABook(String bookName);
     //admin should be able to edit a record(admin only)
     public boolean IncreasingCopiesBook(int bookID);
     //admin increses the book copies(admin only)
